ad840876be7dd079d10c55784b4ed97064bb2dd4
Nick Vilimek <nickvilimek@hotmail.com>
2020-12-13 08:59:07 -0600

    Initial commit for rolling custom git. Just for learning purposes

---
+++ reade.md
@@ -0,0 +1,5 @@
+#Custom Git
+
+Recreating git for learning purporse
+
+Following this tutorial: https://www.leshenko.net/p/ugit/#
---
+++ ugit/base.py
@@ -0,0 +1,139 @@
+import os
+import itertools
+import operator
+
+from collections import namedtuple
+
+from . import data
+
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
+
+def write_tree(directory='.'):
+	entries = []
+	with os.scandir(directory) as it:
+		for entry in it:
+			full = f'{directory}/{entry.name}'
+			if is_ignored(full):
+				continue
+
+			if entry.is_file(follow_symlinks=False):
+				obj_type = 'blob'
+				with open(full, 'rb') as f:
+					oid = data.hash_object(f.read())
+			elif entry.is_dir(follow_symlinks=False):
+				obj_type= 'tree'
+				oid = write_tree(full)
+			entries.append((entry.name, oid, obj_type))
+
+		tree = ''.join(f'{obj_type} {oid} {name}\n'
+				for name, oid, obj_type in sorted(entries))
+
+		return data.hash_object(tree.encode(), 'tree')
+
+def _iter_tree_entries(oid):
+	if not oid:
+		return
+	
+	tree = data.get_object(oid, 'tree')
+	for entry in tree.decode().splitlines():
+		obj_type, oid, name = entry.split(' ', 2)
+		yield obj_type, oid, name
+
+def get_tree(oid, base_path=''):
+	result = {}
+	for obj_type, oid, name in _iter_tree_entries(oid):
+		assert '/' not in name
+		assert name not in ('..', '.')
+		path = base_path + name
+		if obj_type == 'blob':
+			result[path] = oid
+		elif obj_type == 'tree':
+			result.update(get_tree(oid, f'{path}/'))
+		else:
+			assert False, f'Unknown tree entry {obj_type}'
+		
+	return result
+
+def _empty_current_directory():
+	for root, dirnames, filenames in os.walk('.', topdown=False):
+		for filename in filenames:
+			path = os.path.relpath(f'{root}/{filename}')
+			if is_ignored(path) or not os.path.isfile(path):
+				continue
+			os.remove(path)
+		for dirname in dirnames:
+			path = os.path.relpath(f'{root}/{filename}')
+			if is_ignored(path):
+				continue
+			try:
+				os.rmdir(path)
+			except(FileNotFoundError, OSError):
+				pass
+
+def read_tree(tree_oid):
+	_empty_current_directory()
+	for path, oid, in get_tree(tree_oid, base_path='./').items():
+		os.makedirs(os.path.dirname(path), exist_ok=True)
+		with open(path, 'wb') as f:
+			f.write(data.get_object(oid))
+
+def commit(message):
+	commit = f'tree {write_tree()}\n'
+	
+	HEAD = data.get_ref('HEAD')
+	if HEAD:
+		commit += f'parent {HEAD}\n'
+	
+	commit += '\n'
+	commit += f'{message}\n'
+
+	oid = data.hash_object(commit.encode(), 'commit')
+
+	data.update_ref('HEAD',oid)
+
+	return oid
+
+def get_commit(oid):
+	parent = None
+
+	commit = data.get_object(oid, 'commit').decode()
+	lines = iter(commit.splitlines())
+	for line in itertools.takewhile(operator.truth, lines):
+		key, value = line.split(' ', 1)
+		if key == 'tree':
+			tree = value
+		elif key == 'parent':
+			parent = value
+		else:
+			assert False, f'Unkown field {key}'
+
+	message = '\n'.join(lines)
+	return Commit(tree=tree, parent=parent, message=message)
+
+def checkout(oid):
+	commit = get_commit(oid)
+	read_tree(commit.tree)
+	data.update_ref('HEAD', oid)
+
+def get_oid(name):
+	if name == '@': name = 'HEAD'
+
+	refs_to_try = [f'{name}', f'refs/{name}', f'refs/tags/{name}', f'refs/heads/{name}']
+
+	for ref in refs_to_try:
+		if data.get_ref(ref):
+			return data.get_ref(ref)
+
+	is_hex = all(c in string.hexdigits for c in name)
+	if len(name) == 40 and is_hex:
+		return name
+	
+	assert False, f'Unkown name {name}'
+
+def create_tag(name, oid):
+	data.update_ref(f'refs/tags/{name}', oid)
+
+def is_ignored(path):
+	return '.ugit' in path.split('/')
+
+
